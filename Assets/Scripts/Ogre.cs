cf872bec16d6d9ec87c7d03efdac80e2bf07c597
Diogo Sobral <dsforasteiro@gmail.com>
2023-08-19 21:02:30 -0300

    Semana 1 - Input System
    Input System for moving and jumping

--- Assets/Scripts/Ogre.cs
+++ Assets/Scripts/Ogre.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Ogre : Enemy {
-    public Ogre(string name, float life, float damage, float speed) : base(name, life, damage, speed) {}
+// public class Ogre : Enemy {
+//     public Ogre(string name, float life, float damage, float speed) : base(name, life, damage, speed) {}
 
-    public void Move(float direction) {
-        Vector3 movement = new Vector3(direction, 0f, 0f);
-        transform.position += movement * Time.deltaTime * this.Speed;
-        if(direction == 1) this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        else this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
-    }
+//     public void Move(float direction) {
+//         Vector3 movement = new Vector3(direction, 0f, 0f);
+//         transform.position += movement * Time.deltaTime * this.Speed;
+//         if(direction == 1) this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+//         else this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+//     }
 
-    private void OnEnable() {
-        GameManager.Move += Move;
-    }
+//     private void OnEnable() {
+//         GameManager.Move += Move;
+//     }
 
-    private void OnDisable() {
-        GameManager.Move = Move;
-    }
-}
+//     private void OnDisable() {
+//         GameManager.Move = Move;
+//     }
+// }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour {
+    private PlayerControls inputs;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool canJump = true;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float jumpForce = 5f;
+
+    void OnEnable() {
+        inputs.Enable();
+        inputs.Player.Jump.performed += Jump;
+    }
+
+    void OnDisable() {
+        inputs.Disable();
+        inputs.Player.Jump.performed -= Jump;
+    }
+
+    // Start is called before the first frame update
+    void Awake() {
+        inputs = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move(inputs.Player.Move.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 movementDirection) {
+        rb.velocity = new Vector2(movementDirection.x * speed, rb.velocity.y);
+        if(movementDirection.x == 0) animator.SetBool("Walking", false);
+        else {
+            animator.SetBool("Walking", true);
+            if(movementDirection.x < 0) this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            else this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    private void Jump(InputAction.CallbackContext ctx) {
+        if(canJump) {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetBool("Jumping", true);
+            canJump = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground") {
+            canJump = true;
+            animator.SetBool("Jumping", false);
+        }
+    }
+}
